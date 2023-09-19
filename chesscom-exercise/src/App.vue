d24f427f3bc5980f56c73cf71083aaa9877d93fe
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2023-09-19 09:23:40 -0400

    create board

---
+++ chesscom-exercise/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <ChessBoard />
+  </div>
+</template>
+
+<script>
+import ChessBoard from "@/components/ChessBoard.vue";
+ 
+export default {
+  name: "App",
+  components: {
+    ChessBoard,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+
+</style>
---
+++ chesscom-exercise/src/components/ChessBoard.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="chessboard">
+      <div v-for="(row, rowIndex) in board" :key="rowIndex" class="chessboard-row">
+        <div
+          v-for="(cell, colIndex) in row"
+          :key="colIndex"
+          :class="['chessboard-cell', (rowIndex + colIndex) % 2 === 0 ? 'white' : 'black']"
+        >
+          {{ cell }}
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "ChessBoard",
+    data() {
+      return {
+        board: [
+          ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+          ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+          [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+          ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+          ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+        ],
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .chessboard {
+    display: inline-block;
+    border: 1px solid #333;
+  }
+  
+  .chessboard-row {
+    display: flex;
+  }
+  
+  .chessboard-cell {
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 24px;
+  }
+  
+  .white {
+    background-color: #fff;
+  }
+  
+  .black {
+    background-color: #333;
+    color: #fff;
+  }
+  </style>
+  
